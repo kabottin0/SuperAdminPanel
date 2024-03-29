8ce9c23c3a1504619e1580a49adc4fe1f24be648
kabottin0 <gc.eforge@gmail.com>
2024-03-29 12:55:36 +0100

    fix setting send

---
+++ app/src/pages/api/admin/settings/send-settings/index.ts
@@ -0,0 +1,128 @@
+import runMiddleware from "lib/cors";
+import dbConnect from "lib/dbConnect";
+import Products from "models/Products";
+import Cors from "cors";
+import jwtDecode from "jwt-decode";
+import { checkStatus } from "src/utils/checkStatus";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getBlurDataURL } from "src/utils/getBlurDataURL";
+import Categories from "models/Categories";
+import { isValidToken } from "src/utils/jwt";
+import mongoose from "mongoose";
+import Settings from "models/Settings";
+import axios from "axios";
+
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ["GET", "PUT", "POST", "DELETE", "HEAD"],
+});
+
+type Data = {
+  success?: boolean;
+  message?: string;
+  data?: any;
+  count?: number;
+};
+
+/**
+ * Handler function for the product API endpoint.
+ * @param req NextApiRequest object representing the incoming request.
+ * @param res NextApiResponse object representing the outgoing response.
+ */
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  await runMiddleware(req, res, cors);
+
+
+  const {
+    method,
+    query: { id },
+  } = req;
+
+  await dbConnect();
+
+  if (!req.headers.authorization || req.headers.authorization.indexOf('Basic ') === -1) {
+    return res.status(401).json({ message: 'Missing Authorization Header' });
+  }
+
+  // verify auth credentials
+  const base64Credentials = req.headers.authorization.split(' ')[1];
+  const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
+  const [username, password] = credentials.split(':');
+
+  switch (method) {
+    case "GET":
+      try {
+        const settings = await Settings.find(
+          {
+            domain: { $regex: query.search || "", $options: "i" },
+          });
+
+        const webUser = settings[0]?.webUser
+        const webPassword = settings[0]?.webPassword
+        if (username !== webUser || password !== webPassword) {
+          return res.status(401).json({ message: 'Invalid Authentication Credentials' });
+        }
+        res.status(200).json({
+          success: true,
+          data: settings,
+        });
+      } catch (error) {
+        res.status(400).json({ success: false, message: error.message });
+      }
+      break;
+    case "POST":
+
+      try {
+        // if (!authorization) {
+        //   res.status(401).json({ success: false, message: "unauthorized" });
+        //   return;
+        // }
+
+        // Token validation
+        // const isValid = isValidToken(authorization);
+
+        // if (!isValid) {
+        //   res.status(401).json({ success: false, message: "token-expired" });
+        //   return;
+        // }
+          const setting = await Settings.findById(id)
+          if (setting) {
+            const updateSetting = await Settings.findByIdAndUpdate(id, req.body, {
+              new: true,
+              runValidators: true,
+            });
+            const sendSetting = await axios.post('http://localhost:3001/api/admin/send-settings');
+            if (sendSetting.status === 200) {
+              res.status(200).json({ success: true, message: "settings-updated" });
+            } else {
+              res.status(500).json({ success: false, message: "error-sending-to-other-api" });
+            }
+            if (!updateSetting) {
+              return res
+                .status(400)
+                .json({ success: false, message: "item-could-not-found" });
+            }
+            res
+              .status(200)
+              .json({ success: true, message: "settings-updated" });
+          } else {
+            await Settings.create({
+              ...req.body,
+            });
+            await axios.post('http://localhost:3001/api/admin/send-settings');
+          }
+
+
+        res.status(201).json({ success: true, message: "settings-update" });
+      } catch (error) {
+        res.status(400).json({ success: false, message: error.message });
+      }
+      break;
+    default:
+      res.status(400).json({ success: false });
+      break;
+  }
+}
